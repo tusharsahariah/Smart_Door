c401711f1dc9e54ef1aa2fb1ae39c6cd1fe2ea0c
tusharsahariah <tusharkantisahariah2226@gmail.com>
2026-08-07 23:57:40 +0530

    Telegram bot version 1 added

--- src/door/door.ino
+++ src/door/door.ino
@@ -1,13 +1,32 @@
 #include <Keypad.h>
-#include <LiquidCrystal_I2C.h>
-#include <Servo.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+// #include <ESP32Servo.h>
 
-Servo myServo;
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <UniversalTelegramBot.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+
+#define BOT_TOKEN "8929379040:AAFUItIbom8wDeK-k1o15S6bd7yGTAkPa7w"
+#define CHAT_ID "6088702825"
+const char* ssid = "Tushar";
+const char* wifipassword="1234567890";
+
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOT_TOKEN, client);
+
+
+
+// Servo myServo;
 //keypad interfacing
 const byte rows=4;
 const byte cols=4;
-byte rowpins[rows]={9,8,7,6};
-byte colpins[cols]={5,4,3,2};
+byte rowpins[rows]={12,14,27,26};
+byte colpins[cols]={25,33,32,13};
 char keys[rows][cols]={
     {'1', '2', '3', 'A'},
     {'4', '5', '6', 'B'},
@@ -17,11 +36,15 @@ char keys[rows][cols]={
 Keypad k= Keypad(makeKeymap(keys), rowpins, colpins, rows , cols);
 
 //lcd interfacing
-LiquidCrystal_I2C lcd(0x27, 16,2);
+Adafruit_SSD1306 display(
+    SCREEN_WIDTH,
+    SCREEN_HEIGHT,
+    &Wire,
+    -1
+);
 
-
-String password= "12345";
-String enteredPassword= "";
+String password = "12345";
+String enteredPassword = "";
 char key = NO_KEY;
 
 unsigned long stateStartTime=0;
@@ -31,10 +54,10 @@ const unsigned long LOCKED_TIME=30000;
 const unsigned long BUZZER_TIME=5000;
 const unsigned long GREEN_LED_TIME = 2000;
 
-const byte SERVO=10;
-const byte red=13;
-const byte green =12;
-const byte buzzer =11;
+// const byte SERVO=5;
+const byte red=19;
+const byte green =23;
+const byte buzzer =18;
 const byte MAX_ATTEMPTS=3;
 const byte PASSWORD_LENGTH=5;
 byte failedAttempts =0;
@@ -54,57 +77,77 @@ enum State {
 };
 State currentState= STATE_BOOT;
 
+void connectWifi(){
+    WiFi.begin(ssid, wifipassword);
+    while(WiFi.status()!= WL_CONNECTED){
+        delay(500);
+    }
+}
+
+void sendTelegram(String message){
+    bot.sendMessage(CHAT_ID,message,"");
+}
+
 void displayPassword(){
 
-    lcd.setCursor(0,1);
+    display.setCursor(0,16);
     for (int i =0;i<PASSWORD_LENGTH;i++){
         if (i<enteredPassword.length())
-            lcd.print("* ");
+            display.print("* ");
+            
         else
-            lcd.print("_ ");
+            display.print("_ ");
+            
         }
+        display.display();
+
 }
-void Servo_run()
-{
-    for (int pos = 0; pos <= 90; pos++)
-    {
-        myServo.write(pos);
-        delay(15);
-    }
-}
-void Servo_lock()
-{
-    for(int pos=90; pos>=0; pos--)
-    {
-        myServo.write(pos);
-        delay(15);
-    }
-}
+// void Servo_run()
+// {   
+    // for (int pos = 0; pos <= 90; pos++)
+    // {
+    //     myServo.write(pos);
+    //     delay(15);
+    // }
+// }
+// void Servo_lock()
+// {
+//     for(int pos=90; pos>=0; pos--)
+//     {
+//         myServo.write(pos);
+//         delay(15);
+//     }
+// }
 
         
     
 
 void displayState(){
-    lcd.clear();
+    display.clearDisplay();
     switch (currentState){
         case STATE_BOOT:{
-            lcd.setCursor(0,0);
-            lcd.print("Door Lock");
-
-            lcd.setCursor(0,1);
-            lcd.print("Initializing");
+            display.clearDisplay();
+            display.setCursor(0,0);
+            display.print("Door Lock");
+            
+            display.setCursor(0,16);
+            display.print("Initializing");
+            display.display();
         }
             break;
         case STATE_LOCKED:{
-            lcd.setCursor(0,0);
-            lcd.print("Door locked");
-            lcd.setCursor(0,1);
-            lcd.print("press #");
+            display.setCursor(0,0);
+            display.print("Door locked");
+           
+            display.setCursor(0,16);
+            display.print("press #");
+            display.display();
         }
             break;
         case STATE_PASSWORD_ENTRY:{
-            lcd.setCursor(0,0);
-            lcd.print("Enter Password");
+            display.setCursor(0,0);
+            display.print("Enter Password");
+            display.display();
             displayPassword();
         }
             break;
@@ -112,55 +155,84 @@ void displayState(){
             break;
         case STATE_ACCESS_GRANTED:{
             
-            lcd.setCursor(0,0);
-            lcd.print("ACCESS GRANTED");
-            lcd.setCursor(0,1);
-            lcd.print("WELCOME");
+            display.setCursor(0,0);
+            display.print("ACCESS GRANTED");
+            display.display();
+            display.setCursor(0,16);
+            display.print("WELCOME");
+            display.display();
         }
             break;
         case STATE_ACCESS_DENIED:{
-            lcd.setCursor(0,0);
-            lcd.print("Wrong Password");
+            display.setCursor(0,0);
+            display.print("Wrong Password");
+            display.display();
 
-            lcd.setCursor(0,1);
-            lcd.print(MAX_ATTEMPTS-failedAttempts);
-            lcd.print("Left");
+            display.setCursor(0,16);
+            display.print(MAX_ATTEMPTS-failedAttempts);
+            display.display();
+            display.print("Left");
+            display.display();
         }
             break;
         case STATE_UNLOCKING:{
-            lcd.setCursor(0,0);
-            lcd.print("Unlocking");
+            display.setCursor(0,0);
+            display.print("Unlocking");
+            display.display();
         }
             break;
         case STATE_DOOR_OPEN:{
-            lcd.setCursor(0,0);
-            lcd.print("door open");
-            lcd.setCursor(0,1);
-            lcd.print("please enter");
+            display.setCursor(0,0);
+            display.print("door open");
+            display.display();
+            display.setCursor(0,16);
+            display.print("please enter");
+            display.display();
         }
             break;
         case STATE_LOCKING:{
-            lcd.setCursor(0,0);
-            lcd.print("door locking");
+            display.setCursor(0,0);
+            display.print("door locking");
+            display.display();
             
         }
             break;
         case STATE_SYSTEM_LOCKED:{
-            lcd.setCursor(0,0);
-            lcd.print("DOOR LOCKED FOR");
-            lcd.setCursor(0,1);
-            lcd.print("WAIT 30 SEC");
+            display.setCursor(0,0);
+            display.print("DOOR LOCKED FOR");
+            display.display();
+            display.setCursor(0,16);
+            display.print("WAIT 30 SEC");
+            display.display();
         }
 
     }
 }
 
+void oled(){
+    switch(currentState)
+    {
+        case STATE_ACCESS_GRANTED:
+            sendTelegram("🔓 Door Unlocked");
+            break;
 
+        case STATE_ACCESS_DENIED:
+            sendTelegram("❌ Wrong Password");
+            break;
+
+        case STATE_SYSTEM_LOCKED:
+            sendTelegram("🚨 System Locked");
+            break;
+    }
+
+}
 void changeState(State newState){
     currentState=newState;
     stateStartTime=millis();
 
     displayState();
+    oled();
+    
 }
 
 void updateFSM(){
@@ -213,11 +285,12 @@ void updateFSM(){
             break;
         case STATE_ACCESS_GRANTED:{
             digitalWrite(green,HIGH);
-            Servo_run();
+            // Servo_run();
 
             if (millis()-stateStartTime >= GREEN_LED_TIME)
             {   digitalWrite(green,LOW);
                 changeState(STATE_UNLOCKING);}
+        
             
         }
             break;
@@ -230,10 +303,13 @@ void updateFSM(){
             }
             else{
                 digitalWrite(red,HIGH);
-                tone(buzzer, 1000);
+                digitalWrite(buzzer,HIGH);
+                // tone(buzzer, 1000);
                 if(millis()-stateStartTime>=BOOT_TIME)
                 {   digitalWrite(red,LOW);
-                    noTone(buzzer);
+                    digitalWrite(buzzer,LOW);
+                
+                    // noTone(buzzer);
                     changeState(STATE_LOCKED);}
             }
             
@@ -252,7 +328,7 @@ void updateFSM(){
         }
             break;
         case STATE_LOCKING:{
-            Servo_lock();
+            // Servo_lock();
             enteredPassword="";
             changeState(STATE_LOCKED);
             }
@@ -261,10 +337,12 @@ void updateFSM(){
 
         case STATE_SYSTEM_LOCKED:{
             digitalWrite(red,HIGH);
-            tone(buzzer, 1000);
+            digitalWrite(buzzer,HIGH);
+            // tone(buzzer, 1000);
             if (millis()-stateStartTime>= BUZZER_TIME){
                 digitalWrite(red,LOW);
-                noTone(buzzer);
+                // noTone(buzzer);
+                digitalWrite(buzzer,LOW);
             }
             
             if (millis()-stateStartTime>=LOCKED_TIME){
@@ -278,22 +356,43 @@ void updateFSM(){
     }
 }
 
-void setup(){
+void setup() {
+    // myServo.attach(SERVO);
+    // myServo.write(0); 
+    // pinMode(SERVO,OUTPUT);
 
-    myServo.attach(SERVO);
+    pinMode(green, OUTPUT);
+    pinMode(red, OUTPUT);
+    pinMode(buzzer, OUTPUT);
 
-    myServo.write(0);
-    pinMode (green, OUTPUT);
-    pinMode (red, OUTPUT);
-    pinMode (buzzer, OUTPUT);
-    myServo.attach(SERVO);
-    lcd.init();
-    lcd.backlight();
+    digitalWrite(green, LOW);
+    digitalWrite(red, LOW);
+    // digitalWrite(SERVO, LOW);
+    // noTone(buzzer);
+    digitalWrite(buzzer,LOW);
     
-    changeState( STATE_BOOT) ;  
+    Wire.begin(21,22);
+
+    if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
+    {
+        while(true);
+    }
+
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.display();
+    
+
+    client.setInsecure();
+    connectWifi();
+    sendTelegram("🔐 Smart Lock Started");
+    changeState(STATE_BOOT);
 }
 
 void loop(){
     key= k.getKey();
     updateFSM();
 }
+
+
